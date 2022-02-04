d1eecf4f2b5ef080609f682b807e492cadbfaa15
FernandoSullca <42738690+FernandoSullca@users.noreply.github.com>
2022-02-04 18:31:06 -0300

    codigo de lectura de campoos y armado de mensajeria

--- php/contacto.php
+++ php/contacto.php
@@ -1,2 +1,20 @@
+<?php
 /***
 Archivo de configuracion de la mensajeria de los datos */
+//llamada-a-los-campos
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+///datos para el correo 'usuario1@correo.com, usuario2@otrocorreo.com';
+$destinatario  = "pinto.lucas.nahuel@gmail.com , sullcafernando18@gmail.com";
+$asunto = "prueba  de envio de mensaje con php";
+$carte = "De: $nombre $apellido\n";
+$carte .= "Correo: $correo\n";
+$carte .= "Mensaje: $mensaje";
+
+//enviando mensaje
+mail($desinatario,$asunto,$carte);
+header('Location:Confirmacion de envio.html');
+?>
