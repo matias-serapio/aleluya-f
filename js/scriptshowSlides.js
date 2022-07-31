9ee9f1dbb38dee036243bd7aa9442f0d416781a3
Lucas <pinto-lucas@outlook.com>
2022-07-31 04:04:06 +0200

    30-7-22 reestructuración del Body e inserción de scriptshowSlides.js

---
+++ js/scriptshowSlides.js
@@ -0,0 +1,39 @@
+  
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 60000); // Change image every 60000--60 seconds
+}
+
+    // slideIndex = 1;
+    // showSlidesClic(slideIndex);
+    
+    // Next/previous controls
+    function plusSlides(n) {
+      showSlidesClic(slideIndex += n);
+    }
+    
+    function showSlidesClic(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      // let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      // for (i = 0; i < dots.length; i++) {
+      //   dots[i].className = dots[i].className.replace(" active", "");
+      // }
+      slides[slideIndex-1].style.display = "block";
+      // dots[slideIndex-1].className += " active";
+    }
